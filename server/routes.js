fa31796d7149d4c977b8949243b46b462f220e56
Igor Xavier <igorxavier.ixds@gmail.com>
2023-02-15 00:00:02 -0300

    refactor: changed file names

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import ScheduleController from './schedules/ScheduleController.js';
+import ScheduleController from './controllers/ScheduleController.js';
 
 router.get('/schedule', ScheduleController.index);
 router.post('/schedule', ScheduleController.create);
